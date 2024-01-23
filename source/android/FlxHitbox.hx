4192ffb2c8299a7c7ebab7203b855ff2d6303566
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2024-01-23 23:26:11 +0800

    ass

--- source/android/FlxHitbox.hx
+++ source/android/FlxHitbox.hx
@@ -30,29 +30,26 @@ class FlxHitbox extends FlxSpriteGroup {
 		buttonRight = new FlxButton(0, 0);
 
 		hitbox = new FlxSpriteGroup();
-		hitbox.add(add(buttonLeft = createhitbox(0, 0, "left")));
-		hitbox.add(add(buttonDown = createhitbox(320, 0, "down")));
-		hitbox.add(add(buttonUp = createhitbox(640, 0, "up")));
-		hitbox.add(add(buttonRight = createhitbox(960, 0, "right")));
+		hitbox.add(add(buttonLeft = createhitbox(0, 0, 0xC457D3)));
+		hitbox.add(add(buttonDown = createhitbox(320, 0, 0x00DAFF)));
+		hitbox.add(add(buttonUp = createhitbox(640, 0, 0x00FF00)));
+		hitbox.add(add(buttonRight = createhitbox(960, 0, 0XFF0000)));
 
 		var hitbox_hint:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('androidcontrols/hitbox_hint'));
 		hitbox_hint.antialiasing = orgAntialiasing;
 		add(hitbox_hint);
 	}
 
-	public function createhitbox(x:Float = 0, y:Float = 0, frames:String) {
+	public function createhitbox(x:Float = 0, y:Float = 0, colorYeah:Int = 0xFFFFFF) {
 		var button = new FlxButton(x, y);
-		button.loadGraphic(FlxGraphic.fromFrame(getFrames().getByName(frames)));
+		button.loadGraphic(Paths.image('androidcontrols/hitbox'));
 		button.antialiasing = orgAntialiasing;
+		button.color = colorYeah;
 		button.alpha = 0;// sorry but I can't hard lock the hitbox alpha
 		button.onDown.callback = function (){FlxTween.num(0, 0.75, 0.075, {ease:FlxEase.circInOut}, function(alpha:Float){ button.alpha = alpha;});};
 		button.onUp.callback = function (){FlxTween.num(0.75, 0, 0.1, {ease:FlxEase.circInOut}, function(alpha:Float){ button.alpha = alpha;});}
 		button.onOut.callback = function (){FlxTween.num(button.alpha, 0, 0.2, {ease:FlxEase.circInOut}, function(alpha:Float){ button.alpha = alpha;});}
 		return button;
-	}
-
-	public function getFrames():FlxAtlasFrames {
-		return Paths.getSparrowAtlas('androidcontrols/hitbox');
 	}
 
 	override public function destroy():Void {
