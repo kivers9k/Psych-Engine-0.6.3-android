ef240acad475168fe640052e8f1e029d252921f2
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2024-01-24 23:44:57 +0800

    Add files via upload

---
+++ source/MobileConfigState.hx
@@ -0,0 +1,263 @@
+package options;
+
+import flixel.util.FlxColor;
+import flixel.math.FlxPoint;
+import flixel.ui.FlxButton;
+import flixel.text.FlxText;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.transition.FlxTransitionableState;
+import android.FlxHitbox;
+import android.MobileControls.Config;
+import android.FlxVirtualPad;
+
+using StringTools;
+
+class MobileConfigState extends MusicBeatState
+{
+	var vpad:FlxVirtualPad;
+	var hbox:FlxHitbox;
+	var upPozition:FlxText;
+	var downPozition:FlxText;
+	var leftPozition:FlxText;
+	var rightPozition:FlxText;
+	var inputvari:Alphabet;
+	var leftArrow:FlxSprite;
+	var rightArrow:FlxSprite;
+	var saveAndLeave:FlxButton;
+	var controlitems:Array<String> = [
+	    'Pad-Right',
+	    'Pad-Left',
+	    'Pad-Custom',
+	    'Double',
+	    'Hitbox',
+	    'Keyboard'
+	];
+	var curSelected:Int = 0;
+	var buttonistouched:Bool = false;
+	var bindbutton:FlxButton;
+	var config:Config;
+
+	override public function create():Void
+	{
+		super.create();
+		
+		config = new Config();
+		curSelected = config.getcontrolmode();
+
+		var bg:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('menuDesat'));
+		bg.color = 0xFFea71fd;
+		bg.screenCenter();
+		bg.antialiasing = ClientPrefs.globalAntialiasing;
+		add(bg);
+
+		var titleText:Alphabet = new Alphabet(75, 60, "Mobile Config", true);
+		titleText.scaleX = 0.6;
+		titleText.scaleY = 0.6;
+		titleText.alpha = 0.4;
+		add(titleText);
+
+		inputvari = new Alphabet(0, 50, controlitems[curSelected], false);
+		inputvari.x = (FlxG.width - inputvari.width) / 2;
+		add(inputvari);
+
+		var ui_tex = Paths.getSparrowAtlas('androidcontrols/menu/arrows');
+
+		leftArrow = new FlxSprite(inputvari.x - 60, inputvari.y + 50);
+		leftArrow.frames = ui_tex;
+		leftArrow.animation.addByPrefix('idle', "arrow left");
+		leftArrow.animation.addByPrefix('press', "arrow push left");
+		leftArrow.animation.play('idle');
+		add(leftArrow);
+
+		rightArrow = new FlxSprite(inputvari.x + inputvari.width + 10, leftArrow.y);
+		rightArrow.frames = ui_tex;
+		rightArrow.animation.addByPrefix('idle', 'arrow right');
+		rightArrow.animation.addByPrefix('press', "arrow push right", 24, false);
+		rightArrow.animation.play('idle');
+		add(rightArrow);
+
+		upPozition = new FlxText(10, FlxG.height - 104, 0,"Button Up X:" + vpad.buttonUp.x +" Y:" + vpad.buttonUp.y, 16);
+		upPozition.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		upPozition.borderSize = 2.4;
+		add(upPozition);
+
+		downPozition = new FlxText(10, FlxG.height - 84, 0,"Button Down X:" + vpad.buttonDown.x +" Y:" + vpad.buttonDown.y, 16);
+		downPozition.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		downPozition.borderSize = 2.4;
+		add(downPozition);
+
+		leftPozition = new FlxText(10, FlxG.height - 64, 0,"Button Left X:" + vpad.buttonLeft.x +" Y:" + vpad.buttonLeft.y, 16);
+		leftPozition.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		leftPozition.borderSize = 2.4;
+		add(leftPozition);
+
+		rightPozition = new FlxText(10, FlxG.height - 44, 0,"Button RIght x:" + vpad.buttonRight.x +" Y:" + vpad.buttonRight.y, 16);
+		rightPozition.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		rightPozition.borderSize = 2.4;
+		add(rightPozition);
+
+        saveAndLeave = new FlxButton(0,180,'Save and leave',function(){
+			FlxTransitionableState.skipNextTransIn = true;
+			FlxTransitionableState.skipNextTransOut = true;
+			MusicBeatState.switchState(new options.OptionsState());
+        });
+        saveAndLeave.x = (FlxG.width - saveAndLeave.width) / 2 - 50;
+        add(saveAndLeave);
+    
+        reset = new FlxButton(0,180,'Reset',function(){
+            curSelected = 0;
+        });
+        reset.x = (FlxG.width - reset.width) / 2 + 50;
+        add(reset);
+
+		changeSelection();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		leftArrow.x = inputvari.x - 60;
+		rightArrow.x = inputvari.x + inputvari.width + 10;
+		inputvari.x = (FlxG.width - inputvari.width) / 2;
+
+		
+		for (touch in FlxG.touches.list){		
+			if(touch.overlaps(leftArrow) && touch.justPressed)
+			{
+				changeSelection(-1);
+			}
+			else if (touch.overlaps(rightArrow) && touch.justPressed)
+			{
+				changeSelection(1);
+			}
+			trackbutton(touch);
+		}
+	}
+
+	function changeSelection(change:Int = 0)
+	{
+		curSelected += change;
+	
+		if (curSelected < 0)
+			curSelected = controlitems.length - 1;
+		if (curSelected >= controlitems.length)
+			curSelected = 0;
+	
+		inputvari.text = controlitems[curSelected];
+
+		var daChoice:String = controlitems[Math.floor(curSelected)];
+
+		switch (daChoice)
+		{
+			case 'Pad-Right':
+				remove(vpad);
+				vpad = new FlxVirtualPad(RIGHT_FULL, NONE);
+				add(vpad);
+			case 'Pad-Left':
+				remove(vpad);
+				vpad = new FlxVirtualPad(FULL, NONE);
+				add(vpad);
+			case 'Pad-Custom':
+				remove(vpad);
+				vpad = new FlxVirtualPad(RIGHT_FULL, NONE);
+				add(vpad);
+				loadcustom();
+			case 'Double':
+				remove(vpad);
+				vpad = new FlxVirtualPad(DUO, NONE);
+				add(vpad);
+			case 'Hitbox':
+				remove(vpad)
+				hbox = new FlxHitbox();
+				add(hbox);
+			case 'Keyboard':
+				remove(vpad);
+				remove(hbox);
+		}
+
+		if (daChoice != "Hitbox")
+		{
+			hbox.visible = false;
+		}
+
+		if (daChoice != "Pad-Custom")
+		{
+			upPozition.visible = false;
+			downPozition.visible = false;
+			leftPozition.visible = false;
+			rightPozition.visible = false;
+		}
+		else
+		{
+			upPozition.visible = true;
+			downPozition.visible = true;
+			leftPozition.visible = true;
+			rightPozition.visible = true;
+		}
+	}
+
+	function trackbutton(touch:flixel.input.touch.FlxTouch){
+		var daChoice:String = controlitems[Math.floor(curSelected)];
+
+		if (daChoice == 'Pad-Custom'){
+			if (buttonistouched){
+				if (bindbutton.justReleased && touch.justReleased)
+				{
+					bindbutton = null;
+					buttonistouched = false;
+				}else 
+				{
+					movebutton(touch, bindbutton);
+					setbuttontexts();
+				}
+			}
+			else 
+			{
+				if (vpad.buttonUp.justPressed) {
+					movebutton(touch, vpad.buttonUp);
+				}
+				
+				if (vpad.buttonDown.justPressed) {
+					movebutton(touch, vpad.buttonDown);
+				}
+
+				if (vpad.buttonRight.justPressed) {
+					movebutton(touch, vpad.buttonRight);
+				}
+
+				if (vpad.buttonLeft.justPressed) {
+					movebutton(touch, vpad.buttonLeft);
+				}
+			}
+		}
+	}
+
+	function movebutton(touch:flixel.input.touch.FlxTouch, button:flixel.ui.FlxButton) {
+		button.x = touch.x - vpad.buttonUp.width / 2;
+		button.y = touch.y - vpad.buttonUp.height / 2;
+		bindbutton = button;
+		buttonistouched = true;
+	}
+
+	function setbuttontexts() {
+		upPozition.text = "Button Up X:" + vpad.buttonUp.x +" Y:" + vpad.buttonUp.y;
+		downPozition.text = "Button Down X:" + vpad.buttonDown.x +" Y:" + vpad.buttonDown.y;
+		leftPozition.text = "Button Left X:" + vpad.buttonLeft.x +" Y:" + vpad.buttonLeft.y;
+		rightPozition.text = "Button RIght x:" + vpad.buttonRight.x +" Y:" + vpad.buttonRight.y;
+	}
+
+	function save() {
+		config.setcontrolmode(curSelected);
+		var daChoice:String = controlitems[Math.floor(curSelected)];
+
+		if (daChoice == 'Pad-Custom'){
+			config.savecustom(vpad);
+		}
+	}
+
+	function loadcustom():Void{
+		vpad = config.loadcustom(vpad);	
+	}
+}
---
+++ source/MobileSettingSubState.hx
@@ -0,0 +1,61 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+import openfl.Lib;
+
+using StringTools;
+
+class MobileSettingSubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Mobile Setting';
+		rpcTitle = 'Mobile Setting Menu';
+
+		var option:Option = new Option('VirtualPad Opacity',
+		'Changes VirtualPad Opacity',
+		'mobileControlOpacity',
+		'float',
+		0.5);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0.0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+		addOption(option);
+
+		var option:Option = new Option('Hide Hitbox Hint',
+		'if checked, hitbox hint will get hidden',
+		'hideHint',
+		'bool',
+		false);
+		addOption(option);
+
+		super();
+	}
+}
