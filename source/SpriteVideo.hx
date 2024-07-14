7ad33eda7507b707b8f3e9af6398a299ecf3cb10
kivers69 <georgechen1297@gmail.com>
2024-07-14 19:11:26 +0800

    j

--- source/SpriteVideo.hx
+++ source/SpriteVideo.hx
@@ -4,18 +4,20 @@ package;
 import VideoHandler;
 import flixel.graphics.FlxGraphic;
 import flixel.FlxSprite;
+import flixel.FlxG;
 
 class SpriteVideo extends FlxSprite
 {
     public var bitmap:VideoHandler;
-    public var playbackRate:Float = 1;
 
     public function new(x:Float, y:Float, video:String, ?muteSound:Bool = true)
     {
         super(x, y);
         
+        var videoPath:String = Paths.video(video);
+
         bitmap = new VideoHandler();
-        bitmap.playVideo(Paths.video(video), false, muteSound);
+        bitmap.playVideo(videoPath, false, muteSound);
         bitmap.visible = false;
     }
 
@@ -23,10 +25,8 @@ class SpriteVideo extends FlxSprite
     {
         super.update(elapsed);
     
-        var vidBitmap:Graphic = FlxG.bitmap.add(bitmap.bitmapData, false, '');
+        var vidBitmap:FlxGraphic = FlxG.bitmap.add(bitmap.bitmapData, false, '');
         loadGraphic(vidBitmap);
-        
-        bitmap.set_rate(playbackRate);
     }
 
     override function destroy():Void
